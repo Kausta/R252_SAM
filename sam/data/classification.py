371cc0f6c70ead32578f071bc9da9a86ad540cbf
Halfdan Holm <holm.halfdan@gmail.com>
2023-01-05 17:44:44 +0000

    .

--- sam/data/classification.py
+++ sam/data/classification.py
@@ -64,7 +64,7 @@ class ClassificationDataset(data.Dataset):
                 for i in range(len(indicess)):
                     random.shuffle(indicess[i])
                     indicess[i] = indicess[i][:config.data.samples_per_class]
-                indices = list(np.array(indicess).reshape((1, 1500))[0])
+                indices = list(np.array(indicess).reshape((1, 2250))[0])
                 random.shuffle(indices)
                 self.dataset.data = data[indices]
                 self.dataset.targets = list(np.array(targets)[indices])
