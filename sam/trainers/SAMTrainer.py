e45da067763cb0eacfdba14bbc65a5eb6e974334
Halfdan Holm <holm.halfdan@gmail.com>
2023-01-06 13:46:57 +0000

    .

--- sam/trainers/SAMTrainer.py
+++ sam/trainers/SAMTrainer.py
@@ -99,7 +99,7 @@ class SAMTrainer(pl.LightningModule):
             lr_sched.step()
 
         # save checkpoint with pytorch-lightning through WandbLogger callback
-        if self.trainer.current_epoch % self.hparams.train.save_interval == 0:
+        if self.trainer.current_epoch % 1 == 0:
             self.trainer.save_checkpoint(f"checkpoint-{self.trainer.current_epoch}.ckpt")
 
     def validation_step(self, batch, batch_idx):
